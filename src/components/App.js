bf30678bab756e7ad42d001f9e8f7295e54edaef
Prodigy00 <simplygiddy@gmail.com>
2019-07-14 23:43:13 +0100

    hooked up to youtube's data api

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
+import youtube from "../apis/youtube.js";
 
 class App extends Component {
-  //   state = {};
+  state = { videos: [] };
+
+  onSearchSubmit = async userQuery => {
+    const KEY = "AIzaSyAsB7jWvMJcT2IMkQon-XWEOZ7-Lp8yM0E";
+    const response = await youtube.get("/search", {
+      params: {
+        part: "snippet",
+        q: userQuery,
+        key: KEY,
+        maxResults: 5
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+  };
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        {/* custom event handlers can be passed as props with any name you choose when submitted */}
+        <SearchBar onQuerySubmit={this.onSearchSubmit} />I have{" "}
+        {this.state.videos.length} videos.
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,29 @@ import React, { Component } from "react";
 
 class SearchBar extends Component {
   state = { userQuery: "" };
+
+  onInputChange = e => {
+    this.setState({ userQuery: e.target.value });
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+
+    //Make sure we call callback from parent component
+    this.props.onQuerySubmit(this.state.userQuery);
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search</label>
-            <input type="text" value={this.state.userQuery} />
+            <input
+              type="text"
+              value={this.state.userQuery}
+              onChange={this.onInputChange}
+            />
           </div>
         </form>
       </div>
